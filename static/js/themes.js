daabb54bc1e7b082c720e8f3a9be5c9d0e513d98
pyos <pyos100500@gmail.com>
2013-09-19 16:14:00 +0400

    Fix the `style` cookie.

--- static/js/themes.js
+++ static/js/themes.js
@@ -5,7 +5,7 @@ var theme_prefix  = "/static/css/nechan-";
 var theme_postfix = ".css";
 var theme_default = "photon";
 var themes = {
-  "bootstrap": "light",
+  "bootstrap": "white",
   "photon":    "gray",
   "neutron":   "dark"
 };
@@ -36,11 +36,12 @@ $(function () {
     for (var k in themes) {
       var container = $("<li />").attr("class", "board-style-type");
       var child     = $("<a  />").attr("data-style", k).text(k).click(function () {
+        var name = $(this).attr("data-style");
         injected.remove();
-        injected = inject_style($(this).attr("data-style"));
+        injected = inject_style(name);
         $(".board-style-type").removeClass("active");
         $(this).parent().addClass("active");
-        $.cookie("style", k, { "expires": 365, "path": "/" });
+        $.cookie("style", name, { "expires": 365, "path": "/" });
       });
 
       container.append(child).appendTo(options);
