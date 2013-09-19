b2faa852c16cb2abc079af410b1246b3d347f26e
pyos <pyos100500@gmail.com>
2013-09-20 01:59:37 +0400

    Add buttons that expand/collapse truncated messages.

--- static/js/core.js
+++ static/js/core.js
@@ -8,4 +8,24 @@ $(function () {
     );
     return true;
   });
+
+  $(".post-body").each(function () {
+    if (this.offsetHeight < this.scrollHeight) {
+      var min_size = this.offsetHeight;
+      var max_size = this.scrollHeight;
+
+      var self = $(this);
+      var ebtn = self.parents(".post").find(".post-expand-btn").show().click(function () {
+        self.animate({'min-height': max_size}, 300);
+        ebtn.hide();
+        cbtn.show();
+      });
+
+      var cbtn = self.parents(".post").find(".post-collapse-btn").click(function () {
+        self.animate({'min-height': min_size}, 300);
+        cbtn.hide();
+        ebtn.show();
+      });
+    }
+  });
 });
