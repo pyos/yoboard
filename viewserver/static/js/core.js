587f40a27e935ae4022404fb7c35c26f9e54c2d6
pyos <pyos100500@gmail.com>
2013-09-27 23:48:19 +0400

    Rewrite core JS in modular style.

--- viewserver/static/js/core.js
+++ viewserver/static/js/core.js
@@ -1,45 +1,62 @@
 "use strict";
 
-$(function () {
-  $(".thread-view-type").click(function () {
-    $.cookie('view_type', $(this).attr("data-type"), {"expires": 365, "path": "/"});
-    return true;
-  });
 
-  $(".post-content").each(function () {
-    if (this.offsetHeight < this.scrollHeight) {
-      var min_size = this.offsetHeight;
-      var max_size = this.scrollHeight;
-
-      var self = $(this);
-      var ebtn = self.parents(".post").find(".post-expand-btn").show().click(function () {
-        self.animate({'min-height': max_size}, 300);
-        ebtn.hide();
-        cbtn.show();
-      });
-
-      var cbtn = self.parents(".post").find(".post-collapse-btn").click(function () {
-        self.animate({'min-height': min_size}, 300);
-        cbtn.hide();
-        ebtn.show();
+var Core = {
+  enable: function () {
+    if (this === document) {
+      $(".thread-view-type").click(function () {
+        $.cookie('view_type', $(this).attr('data-type'), {"expires": 365, "path": "/"});
+        return true;
       });
     }
-  });
 
-  $(".post-reply-btn").click(function () {
-    $(".post-form:not(.collapse):not(#board-form):not(" + $(this).attr("data-target") + ")").collapse("hide");
-  });
+    $(this).find(".post").each(Core.Posts.entry);
+    $(this).find("[type='file']").each(Core.FileInputs.entry);
+  },
 
-  $("[type='file']").each(function () {
-    var self = $(this);
-    var reset   = null;
-    var display = self.siblings(".adjacent-file-display");
-    var control = self.siblings(".adjacent-file-control");
+  Posts: {
+    entry: function () {
+      var post = $(this);
+      var body = post.find(".post-content");
+      var head = post.find(".media-heading");
 
-    self.change(function () {
-      display.val(self.val().split("\\").pop().split("/").pop());
-    });
+      if (body.innerHeight() < body[0].scrollHeight) {
+        var min_size = body.innerHeight();
+        var max_size = body[0].scrollHeight;
 
-    control.click(function () { self.click(); });
-  });
-});
+        post.addClass("post-expandable");
+        head.find(".post-expand-btn").click(function () {
+          body.animate({'min-height': max_size}, 300);
+          post.addClass('post-expanded');
+        });
+
+        head.find(".post-collapse-btn").click(function () {
+          body.animate({'min-height': min_size}, 300);
+          post.removeClass("post-expanded");
+        });
+      }
+
+      head.find(".post-reply-btn").click(function () {
+        $(".post-form:not(.collapse):not(#board-form):not(" + $(this).attr("data-target") + ")").collapse("hide");
+      });
+    }
+  },
+
+  FileInputs: {
+    entry: function () {
+      var self = $(this);
+      var reset   = null;
+      var display = self.siblings(".adjacent-file-display");
+      var control = self.siblings(".adjacent-file-control");
+
+      self.change(function () {
+        display.val(self.val().split("\\").pop().split("/").pop());
+      });
+
+      control.click(function () { self.click(); });
+    }
+  }
+};
+
+
+$(Core.enable);
