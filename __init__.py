b5a5d33f9c2336b37988199a22d43fd938425e69
pyos <pyos100500@gmail.com>
2013-10-13 18:47:00 +0400

    Automatically create the required directories on startup.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
 import dg
+import os
 import signal
+from . import config
+
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+os.makedirs(config.STORAGE_DIR, 0o755, True)
+os.makedirs(config.UPLOAD_DIR,  0o755, True)
+os.makedirs(config.BANNER_DIR,  0o755, True)
