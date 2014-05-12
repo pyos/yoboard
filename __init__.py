fddf7000c68fc69a807796b4ea920f4473c2174d
pyos <pyos100500@gmail.com>
2014-05-12 17:24:07 +0400

    Do not call `makedirs` if the directory exists.
    That damn "mode 777 != expected mode 755" error. I think it was fixed in 3.4.1...

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ import signal
 from . import config
 
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-os.makedirs(config.STORAGE_DIR, 0o755, True)
-os.makedirs(config.UPLOAD_DIR,  0o755, True)
-os.makedirs(config.BANNER_DIR,  0o755, True)
+os.path.exists(config.STORAGE_DIR) or os.makedirs(config.STORAGE_DIR, 0o755, True)
+os.path.exists(config.UPLOAD_DIR)  or os.makedirs(config.UPLOAD_DIR,  0o755, True)
+os.path.exists(config.BANNER_DIR)  or os.makedirs(config.BANNER_DIR,  0o755, True)
