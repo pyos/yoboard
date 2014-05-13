69ef96076ecafdda2690040c3550993d21adeb37
pyos <pyos100500@gmail.com>
2014-05-13 16:17:36 +0400

    Do lots of stuff.
    Move /static back into /viewserver, remove support for board banners
    (probably need to have a more flexible ad mechanism anyway),
    use abstract UNIX sockets by default (and do not attempt to unlink them.)

--- __init__.py
+++ __init__.py
@@ -1,9 +1,4 @@
 import dg
-import os
 import signal
-from . import config
 
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-os.path.exists(config.STORAGE_DIR) or os.makedirs(config.STORAGE_DIR, 0o755, True)
-os.path.exists(config.UPLOAD_DIR)  or os.makedirs(config.UPLOAD_DIR,  0o755, True)
-os.path.exists(config.BANNER_DIR)  or os.makedirs(config.BANNER_DIR,  0o755, True)
