975dd27c08976917be1eac853caaad1ba8e0cbf3
pyos <pyos100500@gmail.com>
2015-03-07 12:11:12 +0300

    Create UPLOAD_DIR on first import.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
+import os
 import dg
 import signal
 
+from . import config
+
+if not os.path.isdir(config.UPLOAD_DIR):
+    os.makedirs(config.UPLOAD_DIR, 0o755)
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
